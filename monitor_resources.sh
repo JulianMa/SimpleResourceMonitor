6d483594ccb999ee7e72c699b5b17f4ff8b1b25e
Julian Maczuga <r3sist3nt@users.noreply.github.com>
2017-07-06 16:25:03 +0200

    Added absolut memory logging and mem/cpu info

--- monitor_resources.sh
+++ monitor_resources.sh
@@ -1,8 +1,14 @@
 FILE=memory-usage-log.txt
 MAX_PROCESSES=11 #amount of processes to show +1 (Header line)
+echo "System Information:" >> $FILE;
+cat /proc/meminfo >> $FILE;
+cat /proc/cpuinfo >> $FILE;
+
+echo "Starting logging.." >> $FILE;
 while true;
     do
-        ps -e eo pid,%cpu,%mem,uname,args --sort=-%mem | head -n $MAX_PROCESSES >> $FILE
-        echo "----------------------------------------------" >> $FILE;
+        ps -e eo pid,%cpu,%mem,vsz,uname,args --sort=-vsz | head -n $MAX_PROCESSES >> $FILE
         sleep 5;
+        echo "----------------------------------------------" >> $FILE;
+
     done
